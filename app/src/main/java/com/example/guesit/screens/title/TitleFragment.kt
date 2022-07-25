ebaa1977578f7b0c8c8e9d9c4481260ad2370dfd
Earbaj <earbajsaria3@gmail.com>
2022-07-25 08:29:27 -0700

    Creat screens package and inside 3 package game,score,title fragment

---
+++ app/src/main/java/com/example/guesit/screens/title/TitleFragment.kt
@@ -0,0 +1,27 @@
+package com.example.guesit.screens.title
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.guesit.R
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_title, container, false)
+    }
+
+}
