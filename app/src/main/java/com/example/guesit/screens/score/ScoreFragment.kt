5262686304950c829b46d17378aee4f82db50b8e
Earbaj <earbajsaria3@gmail.com>
2022-07-26 10:43:00 -0700

    Add methode to ScoreFragment

--- app/src/main/java/com/example/guesit/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/guesit/screens/score/ScoreFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.guesit.R
+import com.example.guesit.databinding.FragmentScoreBinding
 
 
 /**
@@ -19,8 +23,24 @@ class ScoreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false)
+        // Inflate view and obtain an instance of the binding class.
+        val binding: FragmentScoreBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_score,
+            container,
+            false
+        )
+
+        // Get args using by navArgs property delegate
+        val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
+        binding.scoreText.text = scoreFragmentArgs.score.toString()
+        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        return binding.root
+    }
+
+    private fun onPlayAgain() {
+        findNavController(this).navigate(ScoreFragmentDirections.actionScoreFragmentToGameFragment())
     }
 
 }
